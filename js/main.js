a825398568551c1b7f81a325dc7ae7aa3e87bd5c
makhosi6 <ndondo330@gmail.com>
2020-07-31 20:11:54 +0200

    31/07/2020

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 let url = "http://localhost:9900/get-jobs";
 function getData() {
   fetch(url)
-    .then(resp => 
-      resp.json()
-    )
+    .then(resp => resp.json())
     .then(function(i) {
-     let data = i.splice(400, 300)
+      let data = i.splice(400, 300);
       let wrapper = document.querySelector(".articles");
-data.map((o)=>{
-  wrapper.innerHTML += `<article class="card max bg-light">
+      data.map(o => {
+        wrapper.innerHTML += `<article class="card max bg-light">
     <div class="card-body">
         <h5 class="card-title">${o.title}</h5>
         <p class="card-text">${o.descriptions}</p>
         <a href="#" class="btn btn-primary">Apply</a>
     </div>
   </article>`;
-});
+      });
     })
     .catch(function(error) {
       console.log("ERROR: ", error);
     });
-};
+}
 
 getData();
-
 
 /*
 git add .
